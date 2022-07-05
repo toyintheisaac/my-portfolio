3f3e2dbe468197a3c885077c6d35987212e45241
isaacp.official@gmail.com <58235609+iamisaacp@users.noreply.github.com>
2022-07-05 23:43:39 +0100

    home

--- assets/js/script.js
+++
@@ -1,5 +0,0 @@
-const locate = window.location.host;
-const origin = window.location.origin;
-let webUrl = document.getElementById('webUrl');
-document.getElementById('webUrl').innerHTML = locate;
-webUrl.setAttribute('href', origin);
